ce614c402773b60393502271ef51e83efeeb8360
vianavnxsys <fabioviana918@gmail.com>
2025-05-12 15:32:42 +0000

    novo commit 2

--- cartassupertrunfo.c
+++ cartassupertrunfo.c
@@ -1,36 +1,63 @@
 #include <stdio.h>
 
 int main() {
-   
-// Declaração e inicialização de variáveis com valores padrão
-    char nome_da_cidade[50] = "Campo largo";
-    int codigo_da_cidade = 83602;
-    float populacao =  142695.0;
-    float pib = 4225757.0;
-    float numero_de_pontos_turisticos = 9.0;
+    // Cidade 1
+    char nome_da_cidade1[50] = "Campo Largo";
+    int codigo_da_cidade1 = 83602;
+    float populacao1 = 142695.0;
+    float pib1 = 49000000;
+    float numero_de_pontos_turisticos1 = 9.0;
 
-// Entrada de dados do usuário
-    printf("Digite o nome da cidade: \n");
-    scanf("%s", nome_da_cidade);
+    printf("Nome da cidade 1: \n");
+    scanf(" %[^\n]s", nome_da_cidade1);
 
-    printf("Digite o codigo da cidade: \n");
-    scanf("%d", &codigo_da_cidade);
+    printf("Codigo da cidade: \n");
+    scanf("%d", &codigo_da_cidade1);
 
-    printf("Qual a populaçao da cidade?: \n");
-    scanf("%f", &populacao);
+    printf("Populacao da cidade: \n");
+    scanf("%f", &populacao1);
 
-    printf("Qual o PIB dessa cidade?: \n");
-    scanf("%f", &pib);
+    printf("PIB da cidade: \n");
+    scanf("%f", &pib1);
 
-    printf("Quantos pontos turisticos a cidade tem?: \n");
-    scanf("%f", &numero_de_pontos_turisticos);
+    printf("Pontos turisticos: \n");
+    scanf("%f", &numero_de_pontos_turisticos1);
 
-// Exibição dos dados coletados
-    printf("\n--- Dados da Cidade ---\n");
-    printf("Nome: %s\n", nome_da_cidade);
-    printf("Codigo: %d\n", codigo_da_cidade);
-    printf("Populacao: %.2f\n", populacao);
-    printf("PIB: %.2f\n", pib);
-    printf("Pontos turisticos: %.2f\n", numero_de_pontos_turisticos);
+    printf("\n--- Carta 01 ---\n");
+    printf("Nome: %s\n", nome_da_cidade1);
+    printf("Codigo: %d\n", codigo_da_cidade1);
+    printf("Populacao: %.2f\n", populacao1);
+    printf("PIB: %.2f\n", pib1);
+    printf("Pontos turisticos: %.2f\n", numero_de_pontos_turisticos1);
 
+    // Cidade 2
+    char nome_da_cidade2[50] = "Curitiba";
+    int codigo_da_cidade2 = 875353;
+    float populacao2 = 1773718.0;
+    float pib2 = 49000000;
+    float numero_de_pontos_turisticos2 = 30.0;
+
+    printf("\nNome da cidade 2: \n");
+    scanf("%[^\n]s", nome_da_cidade2);
+
+    printf("Codigo da cidade: \n");
+    scanf("%d", &codigo_da_cidade2);
+
+    printf("Populacao da cidade: \n");
+    scanf("%f", &populacao2);
+
+    printf("PIB da cidade: \n");
+    scanf("%f", &pib2);
+
+    printf("Pontos turisticos: \n");
+    scanf("%f", &numero_de_pontos_turisticos2);
+
+    printf("\n--- Carta 02 ---\n");
+    printf("Nome: %s\n", nome_da_cidade2);
+    printf("Codigo: %d\n", codigo_da_cidade2);
+    printf("Populacao: %.2f\n", populacao2);
+    printf("PIB: %.2f\n", pib2);
+    printf("Pontos turisticos: %.2f\n", numero_de_pontos_turisticos2);
+
+    return 0;
 }
